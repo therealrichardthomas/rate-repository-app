277645e1491cdbd77161306fb2cf26351ecded8c
Richard Thomas <richuthomasp@gmail.com>
2026-01-07 10:24:03 -0500

    10.14

---
+++ src/utils/authStorage.js
@@ -0,0 +1,26 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class AuthStorage {
+  constructor(namespace = 'auth') {
+    this.namespace = namespace;
+  }
+
+  async getAccessToken() {
+    // Get the access token for the storage
+    const token = await AsyncStorage.getItem(`${this.namespace}:accessToken`);
+
+    return token || '';
+  }
+
+  async setAccessToken(accessToken) {
+    // Add the access token to the storage
+    await AsyncStorage.setItem(`${this.namespace}:accessToken`, accessToken);
+  }
+
+  async removeAccessToken() {
+    // Remove the access token from the storage
+    await AsyncStorage.removeItem(`${this.namespace}:accessToken`)
+  }
+}
+
+export default AuthStorage;
